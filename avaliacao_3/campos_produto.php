49eeaf0a6f93c3c3866c99c6728d2348f29074d5
Carla Mello <carlamello@hotmail.com>
2014-01-13 17:24:03 -0200

    Create campos_produto.php

---
+++ avaliacao_3/campos_produto.php
@@ -0,0 +1,32 @@
+
+<input type="text" name="nome" />
+</br>
+<label>Cor: </label>
+<input type="radio" name="cor" value="amarelo" checked="checked" />
+<label>Amarelo</label>
+<input type="radio" name="cor" value="vermelho" />
+<label>Vermelho</label>
+<input type="radio" name="cor" value="rosa" />
+<label>Rosa</label>
+<input type="radio" name="cor" value="verde" />
+<label>Verde</label>
+<input type="radio" name="cor" value="azul" />
+<label>Azul</label>
+<input type="radio" name="cor" value="outros" />
+<label>Outros</label>
+</br>
+<input type="checkbox" name="novo" value="Sim" />
+<label>Produto Novo: </label>
+<br/>
+<label>Prazo de entrega:</label>
+<select name="prazo">
+    <option>05 Dias</option>
+    <option>10 Dias</option>
+    <option>15 Dias</option>
+ 
+    <option></option>
+</select>
+<br/>
+<label>Detalhes: </label>
+<br/>
+<textarea name="descricao" rows="8" cols="20"></textarea>
